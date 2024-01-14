816cbd820b4efa547ab44ec2860bb2f75df2d8bd
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2024-01-15 01:38:20 +0200

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -18,17 +18,15 @@ def do_clean(number=0):
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = int(number)
+    number = 1 if int(number) == 0 else int(number)
 
-    # Delete local archives
-    local_archives = sorted(os.listdir("versions"))
-    [local("rm ./versions/{}".format(a)) for a in local_archives[:-number]]
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-    # Delete remote archives on both web servers
     with cd("/data/web_static/releases"):
-        remote_archives = run("ls -tr").split()
-        remote_archives = [a for a in remote_archives if "web_static_" in a]
-        [run("rm -rf ./{}".format(a)) for a in remote_archives[:-number]]
-
-# Run the script with the given number of archives to keep
-do_clean(2)
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
