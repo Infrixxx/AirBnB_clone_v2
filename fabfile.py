66b7b2292376fc6f44f2ada0953e58a229a16198
Boitumelo <bcrachoshi@gmail.com>
2024-01-15 03:42:13 +0200

    fix

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,7 @@
 # fabfile.py
-from fabric import task
+from invoke import task
 import os.path
 from datetime import datetime
-from fabric.api import local
 
 @task
 def hello(c):
@@ -15,8 +14,8 @@ def hello(c):
                                                          dt.minute,
                                                          dt.second)
     if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
+        if c.local("mkdir -p versions").failed is True:
             return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    if c.local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
     return file
