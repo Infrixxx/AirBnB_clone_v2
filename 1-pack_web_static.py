7a63dbf852b8c98b3236398d6f0845eb252f7fae
Boitumelo <bcrachoshi@gmail.com>
2024-01-15 03:34:20 +0200

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from fabric.api import local
 
 
-@task
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,22 @@
 # fabfile.py
 from fabric import task
+import os.path
+from datetime import datetime
+from fabric.api import local
 
 @task
 def hello(c):
-    print("Hello, Fabric!")
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
