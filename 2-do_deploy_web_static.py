9d1dfce12fdf168ff708870480e9a2af9ee61c00
Boitumelo <bcrachoshi@gmail.com>
2024-01-15 04:22:46 +0200

    indentation fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,11 +6,12 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
+
 env.hosts = ["54.236.43.143", "54.160.124.186"]
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """Distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
@@ -26,5 +27,7 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception as e:
+        print(f"Error: {e}")
         return False
+
