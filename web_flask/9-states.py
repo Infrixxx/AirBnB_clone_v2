699e190c1ed1bc6a24b252f2bcb6d7b3cefbd3ba
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2024-01-29 02:12:38 +0200

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+"""Starts a Flask web application.
 
-from flask import Flask, render_template
-from models import *
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects in DBStorage.
+    /states/<id>: HTML page with information about a specific State.
+"""
 from models import storage
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
+@app.route("/states", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage."""
     states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def state_info(id):
+    """Displays an HTML page with information about a specific State."""
+    state = storage.get("State", id)
+    if state:
+        return render_template("9-states.html", state=state)
+    else:
+        return render_template("9-states.html", not_found=True)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
